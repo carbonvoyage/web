2318757fb91730a44dd29186010e3c5d3847770b
Ari <ari@ceiphr.com>
2022-10-03 10:47:52 -0400

    Created monorepo with basic frontend and extension

---
+++ apps/backend/README.md
@@ -0,0 +1,3 @@
+# Carbon Zero Backend
+
+<!-- TODO -->
---
+++ apps/extension/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./dist/popup.html"],
+    content: ["./src/**/*.{html,js,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
