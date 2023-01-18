4302c095e28ea754066a8e39cf3ade0c81984553
Ari <ari@ceiphr.com>
2023-01-18 13:54:51 -0500

    Fixed eslint and typescript build errors.

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,13 @@
 module.exports = {
-    extends: ["next/babel", "next/core-web-vitals", "turbo", "prettier"],
+    extends: ["next", "turbo", "prettier"],
+    ignorePatterns: ["node_modules", "dist"],
     rules: {
         "@next/next/no-html-link-for-pages": "off",
         "react/jsx-key": "off",
+    },
+    parserOptions: {
+        babelOptions: {
+            presets: [require.resolve("next/babel")],
+        },
     },
 };
